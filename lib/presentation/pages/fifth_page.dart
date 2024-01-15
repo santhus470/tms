39b0ba903cc95ad8ab509715c60678ed8245ea63
santhus470 <santhus470@gmail.com>
2024-01-15 10:25:42 +0530

    dummy vazhipad added

--- lib/presentation/pages/fifth_page.dart
+++ lib/presentation/pages/fifth_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tms/consts/textstyleconst.dart';
 
 class FifthPage extends StatefulWidget {
   const FifthPage({super.key});
@@ -8,8 +9,90 @@ class FifthPage extends StatefulWidget {
 }
 
 class _FifthPageState extends State<FifthPage> {
+  List<String>? vazhipaduList;
+  ScrollController? scrollController;
+  bool showLeftArrow = false;
+  bool showRightArrow = false;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController();
+    scrollController!.addListener(() {
+      setState(() {
+        showLeftArrow = scrollController!.position.pixels > 0;
+        showRightArrow = scrollController!.position.pixels <
+            scrollController!.position.maxScrollExtent;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    vazhipaduList = List.generate(100, (i) => 'അര്‍ച്ചന $i');
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            children: [
+              const Divider(indent: 50, endIndent: 50),
+              const Text(
+                'വഴിപാടുകള്‍',
+                style: bold50Orange,
+              ),
+              SizedBox(
+                height: 500,
+                child: GridView.builder(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  scrollDirection: Axis.horizontal,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 8,
+                      mainAxisSpacing: 25,
+                      childAspectRatio: .15),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.black26,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 20),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            vazhipaduList![index],
+                            // style: normal30,
+                          ),
+                          const Text(
+                            'Rs.150/-',
+                            // style: bold30,
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: vazhipaduList!.length,
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (showLeftArrow)
+          Icon(
+            Icons.arrow_forward_ios,
+            size: 30,
+          ),
+        // Align(
+        //     alignment: Alignment.centerLeft,
+        //     child: AnimatedOpacity(
+        //       opacity: 1,
+        //       duration: const Duration(milliseconds: 500),
+        //       child: Icon(
+        //         Icons.arrow_forward_ios,
+        //         size: 30,
+        //       ),
+        //     ))
+      ],
+    );
   }
 }
--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> {
             ),
             SliverToBoxAdapter(
               child: FifthPage(),
-            )
+            ),
           ],
         ),
       ),
